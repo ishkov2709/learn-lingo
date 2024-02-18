de99556ddcf004bf183917c3ea0211446839d6ba
ishkov2709 <dmitryishkov27@gmail.com>
2024-02-18 22:58:15 +0200

    clean console logs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,14 +8,10 @@ const connection = {
 
 const connect = async () => {
   try {
-    if (connection?.isConnected) {
-      console.log("Connect to DB was earlier");
-      return;
-    }
+    if (connection?.isConnected) return;
 
     const db = await mongoose.connect(DB);
     connection.isConnected = db.connections[0].readyState;
-    console.log("Connect to DB");
   } catch (error) {
     throw new Error("Error in connecting to mongodb");
   }
